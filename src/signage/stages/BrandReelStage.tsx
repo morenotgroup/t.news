0e631b341755d030250a7d9a56ba67ae091bdee4
morenotgroup <gc@agenciataj.com>
2026-04-15 15:40:51 -0300

    Delete src directory

--- src/signage/stages/BrandReelStage.tsx
+++
@@ -1,5 +0,0 @@
-import { StageScaffold } from './StageScaffold';
-
-export function BrandReelStage() {
-  return <StageScaffold title="Brand Reel" subtitle="Vinhetas dinâmicas das quatro marcas em sequência" accent="#88f4ff" />;
-}
--- src/signage/stages/ManifestoStage.tsx
+++
@@ -1,5 +0,0 @@
-import { StageScaffold } from './StageScaffold';
-
-export function ManifestoStage() {
-  return <StageScaffold title="Manifesto" subtitle="Criar experiências que conectam pessoas, marcas e lugares." accent="#f8f8f8" />;
-}
